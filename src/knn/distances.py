d34dfedff123b133ed268fe16d1b89911a3bcd9b
Federica Leacche <federicaleacche01@icloud.com>
2025-12-11 15:48:50 +0100

    feat(distances): add manhattan distance for ordinal feature

--- src/knn/distances.py
+++ src/knn/distances.py
@@ -3,7 +3,16 @@ import numpy as np
 def euclidean_distance(x: np.ndarray, y: np.ndarray) -> float:
     """
     Distanza euclidea (L2). 
-    È la distanza standard richiesta dal professore.
+    È la distanza standard richiesta.
     """
     diff = x - y
     return float(np.sqrt(np.sum(diff * diff)))
+
+def manhattan_distance(x: np.ndarray, y: np.ndarray) -> float:
+    """
+    Distanza di Manhattan (L1).
+    Molto adatta ai dati ordinali 1–10 del dataset.
+    """
+    return float(np.sum(np.abs(x - y)))
+
+
