7bbecfab85a7a7ede44227858992d176ca7c640d
Alessandro Napolitano <alenapolitano2003@gmail.com>
2025-12-19 18:01:49 +0100

    implement edge-case testing for KNN input validation

--- test/knn/test_classifier.py
+++ test/knn/test_classifier.py
@@ -1,10 +1,39 @@
 import unittest
+import numpy as np
+
+from src.knn.classifier import KNNClassifier
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestKNNClassifier(unittest.TestCase):
 
+    def setUp(self):
+        self.X = np.array([
+            [0.0, 0.0],
+            [1.0, 1.0],
+            [2.0, 2.0]
+        ])
+        self.y = np.array([0, 0, 1])
 
-if __name__ == '__main__':
-    unittest.main()
+    # =========================
+    # __init__
+    # =========================
+    def test_invalid_k(self):
+        with self.assertRaises(ValueError):
+            KNNClassifier(k=0)
+
+    def test_invalid_distance_name(self):
+        with self.assertRaises(ValueError):
+            KNNClassifier(distance="invalid")
+
+    # =========================
+    # fit
+    # =========================
+    def test_fit_invalid_X_dim(self):
+        knn = KNNClassifier()
+        with self.assertRaises(ValueError):
+            knn.fit(np.array([1, 2, 3]), self.y)
+
+    def test_fit_invalid_y_dim(self):
+        knn = KNNClassifier()
+        with self.assertRaises(ValueError):
+            knn.fit(self.X, np.array([[0, 1, 1]]))
