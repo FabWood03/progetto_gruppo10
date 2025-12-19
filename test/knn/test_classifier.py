769362c701e4e55f941e039d8794e0133e50da85
Alessandro Napolitano <alenapolitano2003@gmail.com>
2025-12-19 18:05:03 +0100

    add core inference and probability tests for KNNClassifier

--- test/knn/test_classifier.py
+++ test/knn/test_classifier.py
@@ -68,3 +68,58 @@ class TestKNNClassifier(unittest.TestCase):
         knn.fit(self.X, self.y)
         dists = knn._compute_distances(np.array([0, 0]))
         self.assertEqual(dists.shape, (self.X.shape[0],))
+
+
+    # =========================
+    # _vote
+    # =========================
+    def test_vote_no_tie(self):
+        knn = KNNClassifier()
+        labels = np.array([0, 0, 1])
+        pred = knn._vote(labels)
+        self.assertEqual(pred, 0)
+
+    def test_vote_with_tie(self):
+        knn = KNNClassifier(random_state=42)
+        labels = np.array([0, 1])
+        pred = knn._vote(labels)
+        self.assertIn(pred, [0, 1])
+
+    # =========================
+    # predict / predict_one
+    # =========================
+    def test_predict_without_fit(self):
+        knn = KNNClassifier()
+        with self.assertRaises(RuntimeError):
+            knn.predict(np.array([0, 0]))
+
+    def test_predict_one(self):
+        knn = KNNClassifier(k=1)
+        knn.fit(self.X, self.y)
+        pred = knn.predict_one(np.array([0.1, 0.1]))
+        self.assertEqual(pred, 0)
+
+    def test_predict_multiple(self):
+        knn = KNNClassifier(k=1)
+        knn.fit(self.X, self.y)
+        preds = knn.predict(self.X)
+        self.assertEqual(len(preds), len(self.X))
+
+    # =========================
+    # predict_proba
+    # =========================
+    def test_predict_proba_sum_to_one(self):
+        knn = KNNClassifier(k=2)
+        knn.fit(self.X, self.y)
+        proba = knn.predict_proba_one(np.array([0.5, 0.5]))
+        self.assertAlmostEqual(np.sum(proba), 1.0)
+
+    def test_predict_proba_shape(self):
+        knn = KNNClassifier(k=2)
+        knn.fit(self.X, self.y)
+        proba = knn.predict_proba(self.X)
+        self.assertEqual(proba.shape[0], self.X.shape[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
