1178704685564d080aaef3727f1c5dc61f084939
Alessandro Napolitano <alenapolitano2003@gmail.com>
2025-12-19 18:31:20 +0100

    Add unit tests for median-based missing value imputation

--- test/validation/test_base.py
+++ test/validation/test_base.py
@@ -43,3 +43,35 @@ class TestMinMaxScaling(unittest.TestCase):
         # Colonna costante → tutto 0
         self.assertTrue(np.all(X_train_s[:, 0] == 0))
         self.assertTrue(np.all(X_test_s[:, 0] == 0))
+class TestMedianImputation(unittest.TestCase):
+
+    def test_median_imputation_basic(self):
+        X_train = np.array([
+            [1.0, np.nan],
+            [3.0, 4.0]
+        ])
+        X_test = np.array([
+            [np.nan, 2.0]
+        ])
+
+        X_train_i, X_test_i = median_impute_train_test(X_train, X_test)
+
+        # Median of column 0 = 2.0
+        self.assertEqual(X_test_i[0, 0], 2.0)
+
+        # Median of column 1 = 4.0
+        self.assertEqual(X_train_i[0, 1], 4.0)
+
+    def test_no_nan_after_imputation(self):
+        X_train = np.array([
+            [1.0, np.nan],
+            [2.0, 3.0]
+        ])
+        X_test = np.array([
+            [np.nan, np.nan]
+        ])
+
+        X_train_i, X_test_i = median_impute_train_test(X_train, X_test)
+
+        self.assertFalse(np.isnan(X_train_i).any())
+        self.assertFalse(np.isnan(X_test_i).any())
